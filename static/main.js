53aec1d526a4e01823dd8769969ea205a8ae9c8a
kausthubh-coder <kausthubh2007@gmail.com>
2022-04-19 14:03:58 -0400

    fixed search.html

--- static/main.js
+++ static/main.js
@@ -1,12 +1,15 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Search for movie'
+        message: ''
     },
     methods:{
         search: function(){
-            let url = this.message.replace(/\s/g, '-')
+            let url = this.message.replace(/\s/g, '-');
             window.location.replace("/search/"+url);
+        },
+        home: function(){
+            window.location.href = "http://www.movlabs.herokuapp.com";
         }
     }
   })
